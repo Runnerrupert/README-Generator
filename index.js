92f223b32a95111e44886af64fccc81485e68fb1
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-07-23 13:53:23 -0600

    Add prompts for user input in the init function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your Title?", "What is a Description of your Project?", 
+const questions = ["What is your the Title of your Project?", "What is a Description of your Project?", 
     "How do you Install your Project?", "How do you Use your Project?", "What is your Project License?", 
     "Who are your Contributors?", "What are your Tests?", "Questions?"];
 
@@ -11,7 +11,53 @@ const questions = ["What is your Title?", "What is a Description of your Project
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+inquirer .prompt([
+    {
+        name: 'title',
+        type: 'input',
+        message: questions[0],
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: questions[1],
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: questions[2],
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: questions[3],
+    },
+    {
+        name: 'license',
+        type: 'rawlist',
+        message: questions[4],
+        choices: [{name: 'MIT', value: 'mit'}, {name: 'ISC', value: 'isc'},]
+    },
+    {
+        name: 'contributors',
+        type: 'input',
+        message: questions[5],
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: questions[6],
+    },
+    {
+        name: 'questions',
+        type: 'input',
+        message: questions[7],
+    },
+])
+
+}
 
 // Function call to initialize app
 init();
