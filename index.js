a8649e33f9556f8afcf9f20496424fceecc8cf3f
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-07-23 13:46:37 -0600

    Add questions for inquirer prompt, import necessary packages

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your Title?", "What is a Description of your Project?", 
+    "How do you Install your Project?", "How do you Use your Project?", "What is your Project License?", 
+    "Who are your Contributors?", "What are your Tests?", "Questions?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
